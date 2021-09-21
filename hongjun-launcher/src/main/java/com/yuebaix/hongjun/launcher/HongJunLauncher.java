9b4c84a6e7232386150a1dc7c06ec8375f0788e6
yuebaix <yuebaix@outlook.com>
2021-09-21 17:29:05 +0800

    ✨ add scan profile config

--- hongjun-launcher/src/main/java/com/yuebaix/hongjun/launcher/HongJunLauncher.java
+++ hongjun-launcher/src/main/java/com/yuebaix/hongjun/launcher/HongJunLauncher.java
@@ -1,9 +1,22 @@
 package com.yuebaix.hongjun.launcher;
 
+import com.yuebaix.pangu.support.PanGuScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication(scanBasePackages = "com.yuebaix.hongjun")
+@ComponentScan(
+        basePackages = "com.yuebaix.hongjun",
+        useDefaultFilters = false,
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = PanGuScan.IncludeTypeFilter.class)
+        },
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = PanGuScan.ExcludeTypeFilter.class)
+        }
+)
+@SpringBootApplication
 public class HongJunLauncher {
     public static void main(String[] args) {
         SpringApplication.run(HongJunLauncher.class, args);
